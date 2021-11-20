9dc974c2189054761c138599abfe82d3abe6d667
anuryakubu <anuryakubu@gmail.com>
2021-11-20 03:30:29 +0000

    Changes saved

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 const input = document.getElementById('list')
 const addButton = document.getElementById('add')
 const deleteButton = document.getElementById('delete')
-const appendList = document.getElementById('lists')
+const wakeList = document.getElementById('wake')
+const prayList = document.getElementById('pray')
+const meditateList = document.getElementById('meditate')
 const appendChildList = document.getElementById('orderedList')
+
+const List1 = document.getElementById('list1')
+const List2 = document.getElementById('list2')
+const List3 = document.getElementById('list3')
+
 
 addButton.addEventListener('click', add)
 
+
 function add() {
-    appendChildList.appendChild(input.value)
+
+    if (input.value === "") {
+        return;
+    }
+
+    const li = document.createElement('li')
+    li.className = 'lists'
+    const checkbox = document.createElement('input')
+    checkbox.type = "checkbox"
+
+    appendChildList.appendChild(li)
+    
+    li.innerHTML = input.value ;
+    li.appendChild(checkbox)
+    input.value = ""
+    
 }
+
+
