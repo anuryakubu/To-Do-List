6b19b77eb2b6372336a1d41dc11a3862a0a3f4e7
anuryakubu <anuryakubu@gmail.com>
2021-11-25 01:25:51 +0000

    Changes saved

--- main.js
+++ main.js
@@ -10,8 +10,38 @@ const List1 = document.getElementById('list1')
 const List2 = document.getElementById('list2')
 const List3 = document.getElementById('list3')
 
+const check = document.querySelector('input[type="checkbox"]');
+
+console.log(appendChildList)
 
 addButton.addEventListener('click', add)
+
+
+
+ /* check.onchange = () => {
+    if (check.checked) {
+        deleteButton.addEventListener('click', () => {
+            check.parentNode.remove()
+
+        })
+    }
+    
+}*/ 
+
+function deleted(element) {
+    //alert('do something')
+    if (element.checked) {
+       deleteButton.addEventListener('click', () => {
+        element.parentNode.remove()
+       })
+       
+    }
+        
+        
+        
+    
+}
+
 
 
 function add() {
@@ -24,13 +54,16 @@ function add() {
     li.className = 'lists'
     const checkbox = document.createElement('input')
     checkbox.type = "checkbox"
+    checkbox.setAttribute('onchange', 'deleted(this)');
 
     appendChildList.appendChild(li)
     
-    li.innerHTML = input.value ;
+    li.innerHTML = input.value + "  " ;
     li.appendChild(checkbox)
     input.value = ""
+    console.log(appendChildList)
     
 }
+
 
 
